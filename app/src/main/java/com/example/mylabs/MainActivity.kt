ce4f8d7ce9bdcfd909c178e94ce4987c9d34bff3
Thecow-007 <biermandaniel65@gmail.com>
2025-09-15 19:22:35 -0400

    completed lab 2

--- app/src/main/java/com/example/mylabs/MainActivity.kt
+++ app/src/main/java/com/example/mylabs/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.mylabs
 
 import android.os.Bundle
+import android.util.Log
+import android.view.ActionMode
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -11,6 +13,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.mylabs.ui.theme.MyLabsTheme
 
@@ -28,6 +31,7 @@ class MainActivity : ComponentActivity() {
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.w( "MainActivity", "In onCreate() - Loading Widgets" );
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
@@ -42,12 +46,37 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+
+    override fun onStart() {
+        Log.w( "MainActivity", "In onStart() - The activity is now visible on screen" );
+        super.onStart()
+    }
+
+    override fun onResume(){
+        Log.w( "MainActivity", "In onResume() - The activity is now responding to user input" );
+        super.onResume();
+    }
+
+    override fun onPause(){
+        Log.w( "MainActivity", "In onPause()- The activity no longer responds to user input" );
+        super.onPause();
+    }
+
+    override fun onStop(){
+        Log.w( "MainActivity", "In onStop() - The activity is no longer visible" );
+        super.onStop();
+    }
+
+    override fun onDestroy(){
+        Log.w( "MainActivity", "In onDestroy() - Any memory used by the application is freed" );
+        super.onDestroy();
+    }
 }
 
 @Composable
 fun Greeting(name:String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = stringResource(R.string.hello_message),
         modifier = modifier
     )
 }
