8c653e6272e3fea6260ff26a14b1f90ab3d7fa42
Thecow-007 <biermandaniel65@gmail.com>
2025-09-28 22:39:38 -0400

    Followed along with week 3(not really)

--- app/src/main/java/com/example/mylabs/MainActivity.kt
+++ app/src/main/java/com/example/mylabs/MainActivity.kt
@@ -11,6 +11,8 @@ import android.view.ActionMode
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
@@ -23,8 +25,10 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import com.example.mylabs.ui.theme.MyLabsTheme
@@ -102,12 +106,19 @@ fun DisplayLighting(modifier: Modifier = Modifier) {
     }
 
     sensorManager.registerListener(sensorListener, lightingSensor, SensorManager.SENSOR_DELAY_NORMAL)
-    Text(
-        text = "The lighting is ${value.value}",
-        modifier = modifier,
-        fontSize = 40.sp,
-        fontStyle = FontStyle.Bold
-    )
+
+    Column{
+        Image( painter = painterResource( R.drawable.lightbulb ), contentDescription="An image of a lightbulb" )
+        Text(
+            text = "The lighting is ${value.value}",
+            modifier = modifier,
+            fontSize = 40.sp,
+            fontStyle = FontStyle.Italic,
+            fontWeight = FontWeight.Bold
+        )
+    }
+
+
 }
 
 @Preview(showBackground = true)
