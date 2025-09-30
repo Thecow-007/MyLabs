8d1424aa5454a317e48bac4fe7c37ea447acdc21
Thecow-007 <biermandaniel65@gmail.com>
2025-09-29 21:05:43 -0400

    Completed Lab 4

--- app/src/main/java/com/example/mylabs/MainActivity.kt
+++ app/src/main/java/com/example/mylabs/MainActivity.kt
@@ -12,9 +12,15 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
@@ -22,9 +28,11 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
@@ -33,6 +41,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.security.crypto.EncryptedSharedPreferences
 import androidx.security.crypto.MasterKeys
@@ -99,51 +108,71 @@ fun DisplayText(modifier: Modifier = Modifier) {
     val context = LocalContext.current
     val mainKey = MasterKeys.getOrCreate(MasterKeys.AES256_GCM_SPEC)
 
-    val SHARED_PREFS_KEY = "VariableName"
+    val FIRSTNAME_KEY = "FirstName"
+    val LASTNAME_KEY = "LastName"
+    val ADDRESS_KEY = "Address"
 
     var isShowingDialog = remember {mutableStateOf(true)}
 
-    var currentValue = remember {mutableStateOf("Hello World")}
+
 
     var agreeCollectData = remember{mutableStateOf(false) }
 
     val sharedPreferences = EncryptedSharedPreferences.create(
-        "MyFileName" ,
+        "Lab4" ,
         mainKey,
         context,
         EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
         EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
     )
 
-    var storedValue = remember {mutableStateOf(sharedPreferences.getString(SHARED_PREFS_KEY, "Default")) }
+    var firstName = remember {mutableStateOf(sharedPreferences.getString(FIRSTNAME_KEY, "") ?: "")}
+    var lastName = remember {mutableStateOf(sharedPreferences.getString(LASTNAME_KEY, "") ?: "" )}
+    var address = remember {mutableStateOf(sharedPreferences.getString(ADDRESS_KEY, "") ?: "" )}
 
-    fun textBoxValueChanged(newValue: String){
+
+    fun textBoxValueChanged(handler: MutableState<String>, newValue: String, key: String){
         run {
-            currentValue.value = newValue
+            handler.value = newValue
             if (agreeCollectData.value) {
                 with(sharedPreferences.edit())
                 {
-                    putString(SHARED_PREFS_KEY, newValue)
+                    putString(key, newValue)
                     apply()
                 }
             }
         }
     }
 
-    Column {
-        if(agreeCollectData.value){
-            Text(text= "Good news! You let us know where you live!!! :)", modifier = modifier)
-        }
-        else{
-            Text(text= "Hey don't you think its weird that you know where we are but we don't know where you live?? Enter your address.... Please??? ", modifier = modifier)
-        }
-
+    Column (
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(10.dp, Alignment.CenterVertically),
+        modifier = Modifier.padding(24.dp).fillMaxHeight().fillMaxWidth()
+    )
+    {
+        Text("User Info:")
         TextField(
-            value=currentValue.value,
-            onValueChange = {nv -> textBoxValueChanged(nv)}
+            label={
+                Text("First Name:")
+            },
+            value=firstName.value,
+            onValueChange = {nv -> textBoxValueChanged(firstName, nv, FIRSTNAME_KEY)}
+        )
+        TextField(
+            label={
+                Text("Last Name:")
+            },
+            value=lastName.value,
+            onValueChange = {nv -> textBoxValueChanged(lastName, nv, LASTNAME_KEY)}
         )
 
-        Text(text= "Stored Value: ${storedValue.value}", modifier = modifier)
+        TextField(
+            label={
+                Text("Address:")
+            },
+            value=address.value,
+            onValueChange = {nv -> textBoxValueChanged(address, nv, ADDRESS_KEY)}
+        )
 
     }
 
@@ -158,15 +187,38 @@ fun DisplayText(modifier: Modifier = Modifier) {
     fun dismissClicked(){
         agreeCollectData.value = false
         isShowingDialog.value = false
+
+        run {
+            with(sharedPreferences.edit())
+            {
+//                Clear the Encrypted Shared Preferences
+                putString(FIRSTNAME_KEY, "")
+                putString(LASTNAME_KEY, "")
+                putString(ADDRESS_KEY, "")
+                apply()
+            }
+        }
     }
 
     if(isShowingDialog.value)
         AlertDialog(
             onDismissRequest = {isShowingDialog.value = false},
-            title = { Text(text = "Save Address Data?") },
+            title = {
+                Row{
+                    Text(
+                        text = "Let us have your Data?",
+                        fontSize = 20.sp
+                    )
+                    Image(
+                        painter = painterResource( R.drawable.pretty_please ),
+                        contentDescription="Image of a very nice man very nicely asking you for your data",
+                        modifier = Modifier.height(50.dp).width(50.dp)
+                    )
+                }
+            },
             text = { Text("We use your address so that we can plan where to place our next store. This data is optional and not required for our service. ") },       //This below causes a recomposition
-            confirmButton = {  Button( onClick = ::confirmClicked) { Text("I consent to my address being shared.")   }  },
-            dismissButton = {  Button( onClick = ::dismissClicked) {Text("I do not consent to my address being shared.")    }  }
+            confirmButton = {  Button( onClick = ::confirmClicked) { Text("I consent to my data being shared.")   }  },
+            dismissButton = {  Button( onClick = ::dismissClicked) {Text("I do not consent to my data being shared.")    }  }
         )
 
 
